e4baaca9e6804e4bffd3b318c0371f1cd8119b45
Patrick Himmelmann <himmelmannp@gmail.com>
2021-12-02 22:02:18 +0100

    Use rust-tls instead of openssl, use ex ex offers better error handling for io errors

--- src/input.rs
+++ src/input.rs
@@ -1,33 +1,55 @@
-pub type Error = http_req::error::Error;
+#[derive(Debug)]
+pub enum Error {
+    Http(http_req::error::Error),
+    Download(http_req::response::StatusCode),
+    Io(ex::io::Error),
+}
 
+impl From<http_req::error::Error> for Error {
+    fn from(e: http_req::error::Error) -> Self {
+        Self::Http(e)
+    }
+}
+impl From<ex::io::Error> for Error {
+    fn from(e: ex::io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+impl From<http_req::response::StatusCode> for Error {
+    fn from(e: http_req::response::StatusCode) -> Self {
+        Self::Download(e)
+    }
+}
 fn generate_path(day: u32) -> std::path::PathBuf {
     ["input", &day.to_string()].iter().collect()
 }
 fn download_input(day: u32) -> Result<(), Error> {
-    let session = std::fs::read_to_string("cookie")?;
+    let session = ex::fs::read_to_string("cookie")?;
     let cookie = format!("session={}", session);
     let url = format!("https://adventofcode.com/{}/day/{}/input", 2021, day);
     let url_str: &str = &url;
     let uri = http_req::uri::Uri::try_from(url_str).unwrap();
 
     let path = generate_path(day);
-    let mut file = std::fs::File::create(&path)?;
+    let mut file = ex::fs::File::create(&path)?;
     let resp = http_req::request::Request::new(&uri)
         .header("Cookie", &cookie)
         .send(&mut file)?;
-    if !resp.status_code().is_success() {
+    let status = resp.status_code();
+    if !status.is_success() {
         drop(file);
-        std::fs::remove_file(&path)?;
+        ex::fs::remove_file(&path)?;
+        return Err(Error::Download(status));
     }
     Ok(())
 }
-pub fn get_input(day: u32) -> Result<std::fs::File, Error> {
+pub fn get_input(day: u32) -> Result<ex::fs::File, Error> {
     let path = generate_path(day);
-    let file_in = std::fs::File::open(&path);
+    let file_in = ex::fs::File::open(&path);
     if file_in.is_ok() {
         file_in.map_err(|e| e.into())
     } else {
         download_input(day)?;
-        std::fs::File::open(path).map_err(|e| e.into())
+        ex::fs::File::open(path).map_err(|e| e.into())
     }
 }
