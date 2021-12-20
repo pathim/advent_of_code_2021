5c97f7e3e8b82fa32a85a799514f3d314d23124b
Patrick Himmelmann <himmelmannp@gmail.com>
2021-12-21 00:05:00 +0100

    Day 20 Vec instead of HashMap

--- examples/20.rs
+++ examples/20.rs
@@ -1,18 +1,18 @@
 use aoc2021::{get_input, Error};
-use std::{
-    collections::HashMap,
-    io::{BufRead, BufReader},
-};
+use std::io::{BufRead, BufReader};
 
-type Coord = (i64, i64);
-
-fn get_pixel(image: &HashMap<Coord, bool>, algo: &Vec<bool>, (x, y): Coord, outside: bool) -> bool {
+fn get_pixel(image: &Vec<Vec<bool>>, algo: &Vec<bool>, (x, y): (i64, i64), outside: bool) -> bool {
     let mut index = 0;
     for dy in [-1, 0, 1] {
         for dx in [-1, 0, 1] {
-            let c = (x + dx, y + dy);
+            let x_pos = x + dx;
+            let y_pos = y + dy;
             index <<= 1;
-            let val = image.get(&c).copied().unwrap_or(outside);
+            let val = image
+                .get(y_pos as usize)
+                .and_then(|line| line.get(x_pos as usize))
+                .copied()
+                .unwrap_or(outside);
             if val {
                 index |= 1;
             }
@@ -21,24 +21,21 @@ fn get_pixel(image: &HashMap<Coord, bool>, algo: &Vec<bool>, (x, y): Coord, outs
     algo[index]
 }
 
-fn get_size(image: &HashMap<Coord, bool>) -> (Coord, Coord) {
-    let x_min = image.keys().map(|x| x.0).min().unwrap();
-    let x_max = image.keys().map(|x| x.0).max().unwrap();
-    let y_min = image.keys().map(|x| x.1).min().unwrap();
-    let y_max = image.keys().map(|x| x.1).max().unwrap();
-    ((x_min, y_min), (x_max, y_max))
-}
-
-fn enhance(image: &HashMap<Coord, bool>, algo: &Vec<bool>, outside: bool) -> HashMap<Coord, bool> {
-    let ((x_min, y_min), (x_max, y_max)) = get_size(image);
-    let mut new_image = HashMap::new();
-    for x in x_min - 1..=x_max + 1 {
-        for y in y_min - 1..=y_max + 1 {
+fn enhance(image: &Vec<Vec<bool>>, algo: &Vec<bool>, outside: bool) -> Vec<Vec<bool>> {
+    let mut new_image = Vec::new();
+    for y in -1..=image.len() as i64 {
+        let mut nv = Vec::new();
+        for x in -1..=image[0].len() as i64 {
             let coord = (x, y);
-            new_image.insert(coord, get_pixel(image, algo, coord, outside));
+            nv.push(get_pixel(image, algo, coord, outside));
         }
+        new_image.push(nv);
     }
     new_image
+}
+
+fn count_light(image: &Vec<Vec<bool>>) -> usize {
+    image.iter().flatten().filter(|&&x| x).count()
 }
 
 fn main() -> Result<(), Error> {
@@ -52,22 +49,23 @@ fn main() -> Result<(), Error> {
         .map(|c| c == '#')
         .collect::<Vec<_>>();
     lines.next(); // discard empty line
-    let mut image = HashMap::new();
-    for (y, l) in lines.map(|l| l.unwrap()).enumerate() {
-        for (x, c) in l.chars().enumerate() {
-            let coord = (x as i64, y as i64);
-            image.insert(coord, c == '#');
+    let mut image = Vec::new();
+    for l in lines.map(|l| l.unwrap()) {
+        let mut v = Vec::new();
+        for c in l.chars() {
+            v.push(c == '#');
         }
+        image.push(v);
     }
 
     image = enhance(&mut image, &algo, false);
     image = enhance(&mut image, &algo, algo[0]);
-    println!("Answer 1: {}", image.values().filter(|&&x| x).count());
+    println!("Answer 1: {}", count_light(&image));
     for _ in 0..24 {
         image = enhance(&mut image, &algo, false);
         image = enhance(&mut image, &algo, algo[0]);
     }
-    println!("Answer 2: {}", image.values().filter(|&&x| x).count());
+    println!("Answer 1: {}", count_light(&image));
 
     Ok(())
 }
