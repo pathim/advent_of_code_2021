e261aca7bc60fd37b9079b7e5a61e109766daeb3
Patrick Himmelmann <patrick.himmelmann@kit.edu>
2021-12-24 13:48:52 +0100

    Day 22 Part 2

--- examples/22.rs
+++ examples/22.rs
@@ -1,12 +1,12 @@
+#![feature(drain_filter)]
 use aoc2021::{get_input, Error};
 use std::{
-    collections::HashSet,
     io::{BufRead, BufReader},
     num::ParseIntError,
     str::FromStr,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Step {
     turn_on: bool,
     range: [(i64, i64); 3],
@@ -30,28 +30,126 @@ impl FromStr for Step {
 }
 
 impl Step {
-    fn apply_limited(&self, cells: &mut HashSet<(i64, i64, i64)>) {
-        for x in self.range[0].0.max(-50)..self.range[0].1.min(50) {
-            for y in self.range[1].0.max(-50)..self.range[1].1.min(50) {
-                for z in self.range[2].0.max(-50)..self.range[2].1.min(50) {
-                    if self.turn_on {
-                        cells.insert((x, y, z));
-                    } else {
-                        cells.remove(&(x, y, z));
-                    }
-                }
+    fn limit(mut self) -> Self {
+        for (min, max) in self.range.iter_mut() {
+            *min = (*min).max(-50);
+            *max = (*max).min(50);
+        }
+        self
+    }
+
+    fn intersects(&self, other: &Self) -> bool {
+        for (s, o) in self.range.iter().zip(&other.range) {
+            if s.0 >= o.1 || s.1 <= o.0 {
+                return false;
             }
         }
+        true
+    }
+
+    fn volume(&self) -> i64 {
+        self.range
+            .iter()
+            .map(|(min, max)| (max - min).max(0))
+            .product()
+    }
+
+    fn apply_vec(self, v: &mut Vec<Step>) {
+        if self.turn_on {
+            self.union_vec(v);
+        } else {
+            self.difference_vec(v);
+        }
+    }
+
+    fn union_vec(self, v: &mut Vec<Step>) {
+        assert!(self.turn_on);
+        let inter_idx = v.iter().position(|x| self.intersects(x));
+        if let Some(inter_idx) = inter_idx {
+            let inter = v.swap_remove(inter_idx);
+            for n in inter.union(self) {
+                n.union_vec(v);
+            }
+        } else {
+            v.push(self);
+        }
+    }
+    fn difference_vec(self, v: &mut Vec<Step>) {
+        assert!(!self.turn_on);
+        while let Some(inter_idx) = v.iter().position(|x| self.intersects(x)) {
+            let inter = v.swap_remove(inter_idx);
+            v.append(&mut inter.difference(self.clone()));
+        }
+    }
+
+    fn union(self, other: Self) -> Vec<Self> {
+        assert!(self.turn_on);
+        assert!(other.turn_on);
+        let mut res = Vec::new();
+        if !self.intersects(&other) {
+            res.push(self);
+            res.push(other);
+            return res;
+        }
+        let (smaller, larger) = if self.volume() > other.volume() {
+            (other, self)
+        } else {
+            (self, other)
+        };
+        for i in 0..smaller.range.len() {
+            let mut new_step = smaller.clone();
+            new_step.range[i].1 = larger.range[i].0;
+            if new_step.volume() > 0 {
+                new_step.union_vec(&mut res);
+            }
+            let mut new_step = smaller.clone();
+            new_step.range[i].0 = larger.range[i].1;
+            if new_step.volume() > 0 {
+                new_step.union_vec(&mut res);
+            }
+        }
+        res.push(larger);
+        res
+    }
+    fn difference(self, other: Self) -> Vec<Self> {
+        assert!(self.turn_on);
+        assert!(!other.turn_on);
+        let mut res = Vec::new();
+        if !self.intersects(&other) {
+            res.push(self);
+            return res;
+        }
+        let (smaller, larger) = (self, other);
+        for i in 0..smaller.range.len() {
+            let mut new_step = smaller.clone();
+            new_step.range[i].1 = larger.range[i].0;
+            if new_step.volume() > 0 {
+                new_step.union_vec(&mut res);
+            }
+            let mut new_step = smaller.clone();
+            new_step.range[i].0 = larger.range[i].1;
+            if new_step.volume() > 0 {
+                new_step.union_vec(&mut res);
+            }
+        }
+
+        res
     }
 }
 fn main() -> Result<(), Error> {
     let input = BufReader::new(get_input(22)?);
-    let sequence = input.lines().map(|l| l.unwrap().parse::<Step>().unwrap());
-    let mut res = HashSet::new();
-    for step in sequence {
-        step.apply_limited(&mut res);
+    let sequence = input
+        .lines()
+        .map(|l| l.unwrap().parse::<Step>().unwrap())
+        .collect::<Vec<_>>();
+    let mut res1 = Vec::new();
+    let mut res2 = Vec::new();
+    for s in sequence {
+        s.clone().limit().apply_vec(&mut res1);
+        s.apply_vec(&mut res2);
     }
+    println!("Answer 1: {}", res1.iter().map(|x| x.volume()).sum::<i64>());
+    println!("Answer 2: {}", res2.iter().map(|x| x.volume()).sum::<i64>());
 
-    println!("Answer 1: {}", res.len());
     Ok(())
 }
